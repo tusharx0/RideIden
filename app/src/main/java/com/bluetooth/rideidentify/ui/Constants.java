be66d633a9150a8954e95577e62312e6760fff81
zerotransit <zerotransit@gmail.com>
2018-07-02 13:22:55 +0530

    Initial commit

---
+++ app/src/main/java/com/bluetooth/rideidentify/ui/Constants.java
@@ -0,0 +1,8 @@
+package com.bluetooth.rideidentify.ui;
+
+public class Constants {
+    public static final String TAG = "bdsk";
+    public static final String FIND = "Search Passengers";
+    public static final String STOP_SCANNING = "Stopped";
+    public static final String SCANNING = "Searching ";
+}
---
+++ app/src/main/java/com/bluetooth/rideidentify/ui/PeripheralControlActivity.java
@@ -0,0 +1,6 @@
+package com.bluetooth.rideidentify.ui;
+
+public class PeripheralControlActivity {
+    public static final String EXTRA_NAME = "name";
+    public static final String EXTRA_ID = "id";
+}
